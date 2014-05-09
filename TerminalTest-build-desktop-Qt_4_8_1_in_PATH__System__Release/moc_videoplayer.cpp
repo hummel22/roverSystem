978224966b3b759337d4dd3c7f2d6e8becaa38db
Hummel <hummel.kevin@gmail.com>
2014-05-09 18:18:40 -0400

    added streamtesting bash files

--- TerminalTest-build-desktop-Qt_4_8_1_in_PATH__System__Release/moc_videoplayer.cpp
+++ TerminalTest-build-desktop-Qt_4_8_1_in_PATH__System__Release/moc_videoplayer.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'videoplayer.h'
 **
-** Created: Fri May 9 17:48:45 2014
+** Created: Fri May 9 18:16:47 2014
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
 **
 ** WARNING! All changes made in this file will be lost!
